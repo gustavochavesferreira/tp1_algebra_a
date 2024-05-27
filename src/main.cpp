7f69466a742a4a95e56bbe59395555b368bf5522
Samuel Viana <samuelsnv2@gmail.com>
2024-05-27 18:54:46 +0000

    change on main - commented

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,11 @@ int main() {
 
     /*
     mpz_int N; cin >> N;
+    
     mpz_int next_prime = find_next_prime(N);
-    mpz_int generator = find_generator(next_prime);
-
     cout << "O menor primo maior do que " << N << " é " << next_prime << endl;
+
+    mpz_int generator = find_generator(next_prime);
     cout << "Um gerador de Z(" << next_prime << ") é " << generator << endl;
     */
     
